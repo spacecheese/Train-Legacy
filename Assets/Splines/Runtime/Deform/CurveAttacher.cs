5e640f8812383657f076225b5fb75afea1fce909
spacecheese <12695808+spacecheese@users.noreply.github.com>
2020-07-17 19:35:01 +0100

    Rewrite CurveAttacher

--- Assets/Splines/Runtime/Deform/CurveAttacher.cs
+++ Assets/Splines/Runtime/Deform/CurveAttacher.cs
@@ -13,83 +13,111 @@ namespace Splines.Deform
     /// </summary>
     /// <typeparam name="TAttachment">The type to attach to each curve</typeparam>
     [ExecuteAlways]
-    public abstract class CurveAttacher<TAttachment> : MonoBehaviour, ISerializationCallbackReceiver 
-        where TAttachment : class
+    [Serializable]
+    public abstract class CurveAttacher<TAttachment>
     {
-        [SerializeField]
-        protected Spline spline = null;
-
-        private readonly Dictionary<Curve, TAttachment> attachments = new Dictionary<Curve, TAttachment>();
-        private readonly List<Action> waitingActions = new List<Action>();
-
-        protected abstract TAttachment CurveAdded(Curve curve);
-        protected abstract void CurveChanged(Curve curve, ref TAttachment attachment);
-        protected abstract void CurveRemoved(TAttachment attachment);
-
-        private void RegisterSpline()
-        {
-            foreach (var curve in spline.Curves)
-                AddCurve(curve);
-
-            spline.Curves.ItemAdded += (s, e) => AddCurve(e.Item);
-            spline.Curves.ItemRemoved += (s, e) => RemoveCurve(e.Item);
+        private Spline spline = null;
+        public Spline Spline {
+            get { return spline; }
+            set { Spline oldSpline = spline; spline = value; OnSplineChanged(oldSpline, spline); }
         }
 
-        private void AddCurve(Curve curve)
+        private readonly List<TAttachment> attachments = null;
+        public IReadOnlyList<TAttachment> Attachments => attachments;
+
+        // Implementation defined handlers for managing attachments.
+        /// <summary>
+        /// Called when a curve is added or inserted into the tracked spline.
+        /// </summary>
+        protected abstract TAttachment OnCurveAdded(Curve curve);
+        /// <summary>
+        /// Called when a curve is modified or replaced in the tracked spline.
+        /// </summary>
+        protected abstract TAttachment OnCurveChanged(Curve curve, TAttachment attachment);
+        /// <summary>
+        /// Called when a curve is removed from the tracked spline.
+        /// </summary>
+        protected abstract void OnCurveRemoved(TAttachment attachment);
+
+        // Internal handlers for curve related events.
+        private void OnSplineCurveAdded(object sender, ListModifiedEventArgs<Curve> e)
         {
-            // Queue the curve for addition.
-            waitingActions.Add(() => CurveAdded(curve));
-            curve.CurveChanged += OnCurveChanged;
+            e.Item.CurveChanged += OnSplineCurveChanged;
+            attachments.Add(OnCurveAdded(e.Item));
         }
 
-        private void RemoveCurve(Curve curve)
+        private void OnSplineCurveInserted(object sender, ListModifiedEventArgs<Curve> e)
         {
-            TAttachment attachment = attachments[curve];
-            curve.CurveChanged -= OnCurveChanged;
-            // Queue the curve for removal.
-            waitingActions.Add(() => CurveRemoved(attachment));
-
-            attachments.Remove(curve);
+            e.Item.CurveChanged += OnSplineCurveChanged;
+            attachments.Insert(e.Index, OnCurveAdded(e.Item));
         }
 
-        private void OnCurveChanged(object sender, EventArgs evt)
+        private void OnSplineCurveReplaced(object sender, ListItemReplacedEventArgs<Curve> e)
         {
-            // Queue the curve for renewal in the next update.
-            waitingActions.Add(() =>
+            e.OldItem.CurveChanged -= OnSplineCurveChanged;
+            attachments[e.Index] = OnCurveChanged(e.Item, attachments[e.Index]);
+            e.Item.CurveChanged += OnSplineCurveChanged;
+        }
+
+        private void OnSplineCurveChanged(object sender, EventArgs e)
+        {
+            Curve curve = (Curve)sender;
+            int index = spline.Curves.IndexOf(curve);
+            OnCurveChanged(curve, attachments[index]);
+        }
+
+        private void OnSplineCurveRemoved(object sender, ListModifiedEventArgs<Curve> e)
+        {
+            e.Item.CurveChanged -= OnSplineCurveChanged;
+            OnCurveRemoved(attachments[e.Index]);
+            attachments.RemoveAt(e.Index);
+        }
+
+        private void OnSplineCurvesCleared(object sender, EventArgs e)
+        {
+            foreach (var attachment in attachments)
+                OnCurveRemoved(attachment);
+
+            attachments.Clear();
+        }
+
+        private void OnSplineChanged(Spline oldValue, Spline newValue)
+        {
+            if (oldValue == newValue) return;
+
+            if (oldValue != null)
             {
-                var curve = sender as Curve;
+                oldValue.Curves.ItemAdded -= OnSplineCurveAdded;
+                oldValue.Curves.ItemInserted -= OnSplineCurveInserted;
+                oldValue.Curves.ItemReplaced -= OnSplineCurveReplaced;
+                oldValue.Curves.ItemRemoved -= OnSplineCurveRemoved;
+                oldValue.Curves.Cleared -= OnSplineCurvesCleared;
 
-                var attachment = attachments[curve];
-                CurveChanged(curve, ref attachment);
-                attachments[curve] = attachment;
-            });
-        }
+                int i = 0;
+                foreach (var curve in oldValue.Curves)
+                {
+                    curve.CurveChanged -= OnSplineCurveChanged;
+                    OnCurveRemoved(attachments[i++]);
+                }
+                    
+                attachments.Clear();
+            }
+            
 
-        public void OnBeforeSerialize()
-        {
-            // Remove the curves directly as no OnUpdate will be called.
-            foreach (var attachment in attachments.Values)
-                CurveRemoved(attachment);
-        }
+            if (newValue != null)
+            {
+                newValue.Curves.ItemAdded += OnSplineCurveAdded;
+                newValue.Curves.ItemInserted += OnSplineCurveInserted;
+                newValue.Curves.ItemReplaced += OnSplineCurveReplaced;
+                newValue.Curves.ItemRemoved += OnSplineCurveRemoved;
+                newValue.Curves.Cleared += OnSplineCurvesCleared;
 
-        public void OnAfterDeserialize() { }
-
-        private Spline oldSpline = null;
-
-        private void OnValidate()
-        {
-            if (spline == null ||
-                spline == oldSpline) 
-                return;
-
-            // Only re-register the spline if it has changed.
-            RegisterSpline();
-        }
-
-        private void Update()
-        {
-            foreach (var action in waitingActions)
-                action?.Invoke();
+                foreach (var curve in newValue.Curves)
+                {
+                    curve.CurveChanged += OnSplineCurveChanged;
+                    attachments.Add(OnCurveAdded(curve));
+                }
+            }
         }
     }
 }
