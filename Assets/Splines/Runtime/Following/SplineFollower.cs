0240666dc6873d37a6f43211cb48803caed78da3
spacecheese <12695808+spacecheese@users.noreply.github.com>
2020-07-28 19:16:24 +0100

    Implement SplineFollower

---
+++ Assets/Splines/Runtime/Following/SplineFollower.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Splines
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class SplineFollower : MonoBehaviour
+    {
+        [SerializeField]
+        Spline spline;
+
+        [SerializeField]
+        float distance = 0;
+
+        float curveVelocity = 0;
+        Rigidbody body;
+
+        private void Start()
+        {
+            body = GetComponent<Rigidbody>();
+            body.freezeRotation = true;
+        }
+
+        private void FixedUpdate()
+        {
+            if (spline == null)
+                return;
+
+            Vector3 curveNormal = spline.GetNormalAtDistance(distance).normalized;
+            Quaternion rotation = spline.GetRotationAtDistance(distance);
+
+            curveVelocity = Vector3.Dot(body.velocity, curveNormal);
+            // Only keep the portion of the body velocity acting along the spline.
+            body.velocity = curveVelocity * curveNormal;
+
+            transform.position = spline.GetPositionAtDistance(distance);
+            transform.rotation = rotation;
+
+            // Move along the curve at the curve velocity.
+            distance += curveVelocity * Time.deltaTime;
+
+            // Stop the follower from leaving the spline.
+            if (distance < 0)
+            {
+                distance = 0;
+                body.velocity = -body.velocity;
+            }
+            else if (distance > spline.Length)
+            {
+                distance = spline.Length;
+                body.velocity = -body.velocity;
+            }
+        }
+    }
+}
